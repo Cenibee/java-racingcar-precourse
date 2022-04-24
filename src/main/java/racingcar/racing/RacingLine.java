076b2fbd0d019659c42c70128a9886b2c2adabd4
cenibee <ksj3452@naver.com>
2022-04-24 18:21:10 +0900

    feat: 레이싱 라인 전진 기능 구현

--- src/main/java/racingcar/racing/RacingLine.java
+++ src/main/java/racingcar/racing/RacingLine.java
@@ -1,8 +1,13 @@
 package racingcar.racing;
 
+import camp.nextstep.edu.missionutils.Randoms;
 import racingcar.car.Car;
 
 public class RacingLine {
+
+    public static final int MIN_RANDOM = 0;
+    public static final int MAX_RANDOM = 9;
+    public static final int MIN_GO_STRAIGHT = 4;
 
     private final Car racingCar;
     private int position;
@@ -22,13 +27,23 @@ public class RacingLine {
         return position;
     }
 
+    public String getRacingCarName() {
+        return this.racingCar.getName();
+    }
+
+    int getRandom() {
+        return Randoms.pickNumberInRange(MIN_RANDOM, MAX_RANDOM);
+    }
+
+    public void goStraight() {
+        if (getRandom() >= MIN_GO_STRAIGHT) {
+            position++;
+        }
+    }
+
     private void assertRacingCar(Car car) {
         if (car == null) {
             throw new IllegalArgumentException("자동차는 null 일 수 없습니다");
         }
-    }
-
-    public String getRacingCarName() {
-        return this.racingCar.getName();
     }
 }
